0f4568a75755168b6bc53ccdf236ed291ebe9947
mostafa <mostafasharifi64@gmail.com>
2022-12-17 10:53:56 -0500

    version

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("First Version Is complete");
+        System.out.println("hello everyone");
+    }
+}
